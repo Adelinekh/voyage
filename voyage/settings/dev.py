5ecf17c3fdf3166db9bb66d61bba81f973cea14c
Adeline El Khoury <adelineinati@gmail.com>
2022-10-13 09:18:50 +0200

    début

---
+++ voyage/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-sh(t)j=2loa2($bn7p0-1pf9n46og77rd-dfo5#s31_dc!g)k^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
